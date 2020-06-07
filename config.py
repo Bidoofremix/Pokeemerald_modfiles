81766fcc75ef18c9d49a6ad34351af12c9267c15
Bidoofremix <bidoofremix@gmail.com>
2020-06-07 12:29:13 +0300

    update base path in config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 
-base_dir = "C:\\Users\Miro\Documents\Pokeemerald\pokeemerald_apr20"
+base_dir = "C:\\Users\Miro\Documents\Pokeemerald\pokeemerald_june20"
 pokeemerald_dir = "{0}\pokeemerald_mod".format(base_dir)
 vanilla_dir = "{0}\pokeemerald".format(base_dir)
