2593684d1be471f3a3918cc3ca6329033566cd3b
Bidoofremix <bidoofremix@gmail.com>
2020-06-07 19:59:33 +0300

    batch modify trainers with excel file

--- modify_pokeemerald.py
+++ modify_pokeemerald.py
@@ -40,7 +40,7 @@ for i in range(0,xl_sheet.nrows):
 	row = [c.value for c in xl_sheet.row(i)]
 	
 	# new species found, save previous
-	if row[0].startswith("SPECIES_"):
+	if row[0].startswith("SPECIES_") or i == xl_sheet.nrows-1:
 		if new_data != "":
 			pokemon_data[species_name] = new_data
 		species_name = row[0]
@@ -51,7 +51,8 @@ for i in range(0,xl_sheet.nrows):
 		new_data[row[0]] = row[1]
 
 # save last species
-pokemon_data[species_name] = new_data
+#pokemon_data[species_name] = new_data
+
 
 ########## replace rows with new data
 
@@ -110,7 +111,7 @@ raw_folder = "raw"
 
 for dir, subdirs, files in os.walk(raw_folder):
 	for fname in files:
-
+	
 		mod_path = "{0}\{1}".format(dir,fname)
 		pokeemerald_path = pokeemerald_dir + mod_path[len(raw_folder):]
 		
@@ -210,11 +211,247 @@ for dir, subdirs, files in os.walk(raw_folder):
 			for line in original_lines:
 				f.write(line + "\n")
 		
+########## trainer data
+
+print("\nvalidate and modify trainers")
+
+trainer_data = {}
+
+# defined trainers and their party offsets
+defined_trainers = {}
+party = ""
+trainer_file = "{0}/src/data/trainers.h".format(pokeemerald_dir)
+trainer_file_lines = []
+with open(trainer_file, "r") as f:
+	for line in f:
+		trainer_file_lines.append(line)
+		if line.startswith("    [TRAINER_"):
+			if party != "":
+				defined_trainers[current_trainer] = party
+			current_trainer = line.lstrip(" ").split("]")[0].lstrip("[")
+		else:
+			if ".party = {" in line:
+				party = line.lstrip(" ").split(" ")[4].split("}")[0]
+				
+# defined moves
+defined_moves = set([])
+move_file = "{0}/include/constants/moves.h".format(pokeemerald_dir)
+with open(move_file, "r") as f:
+	for line in f:
+		if line.startswith("#define MOVE_"):
+			defined_moves.add(line.split(" ")[1])
+
+# defined species
+defined_species = set([])
+species_file = "{0}/include/constants/species.h".format(pokeemerald_dir)
+with open(species_file, "r") as f:
+	for line in f:
+		if line.startswith("#define SPECIES_"):
+			defined_species.add(line.split(" ")[1])
+
+# defined items
+defined_items = set([])
+items_file = "{0}/include/constants/items.h".format(pokeemerald_dir)
+with open(items_file, "r") as f:
+	for line in f:
+		if line.startswith("#define ITEM_"):
+			defined_items.add(line.split(" ")[1])			
+			
+trainer_excel = "trainers/trainers.xlsx"
+
+xl_workbook = xlrd.open_workbook(trainer_excel)
+xl_sheet = xl_workbook.sheet_by_index(0)
+
+party = ""
+for i in range(0,xl_sheet.nrows):
+	row = [c.value for c in xl_sheet.row(i)]
+	row = [int(c) if isinstance(c, float) else c for c in row]
+	
+	# new trainer or last line of file
+	if row[0].startswith("TRAINER_") or i == xl_sheet.nrows-1:
+		if party != "":
+		
+			# validate level and IV
+			for i,mon in enumerate(mons):
+			
+				if not isinstance(mon["lvl"], int):
+					print("\nerror: non-numeric level '{0}' ({1})".format(\
+						mon["lvl"],trainer))
+						
+				if mon["iv"] == "":
+					mon["iv"] = 0
+				else:
+					if not isinstance(mon["iv"],int):
+						print("\nerror: non-numeric IV '{0}' ({1})".format(\
+							mon["iv"],trainer))
+				
+				mons[i] = mon
+		
+			# validate items
+			items = [mon["heldItem"] for mon in mons]
+			if any(items):
+				mon_items = True
+				for i,mon in enumerate(mons):
+				
+					if mon["heldItem"] != "":
+						tmp_item = "ITEM_%s" % mon["heldItem"].replace(" ","_").upper()
+						if tmp_item not in defined_items:
+							print("\nerror: item {0} not defined ({1})".format(tmp_item,trainer))
+							exit(0)
+					else:
+						tmp_item = "ITEM_NONE"
+					mon["heldItem"] = tmp_item
+					mons[i] = mon
+			else:
+				mon_items = False
+				
+			# validate moves
+			moves = [mon["moves"] for mon in mons]
+			if any(moves):
+				if not all(moves):
+					print("\nerror: some, but not all mons of %s have moves" % trainer)
+					exit(0)
+				mon_moves = True
+				for i,mon in enumerate(mons):
+					tmp_moves = mon["moves"].split(",")
+					tmp_moves = set(["MOVE_%s" % m.strip().replace(" ","_").upper() \
+						for m in tmp_moves])
+					if not tmp_moves.issubset(defined_moves):
+						print("\nerror: not all moves of %s defined:" % trainer)
+						[print(m) for m in sorted(tmp_moves)]
+						exit(0)
+					tmp_moves = list(tmp_moves)
+					# fill missing slots with MOVE_NONE
+					if len(tmp_moves) != 4:
+						tmp_moves += ["MOVE_NONE"] * (4 - (len(tmp_moves)))
+					mon["moves"] = tmp_moves
+					mons[i] = mon
+			else:
+				mon_moves = False
+			
+			# validate species
+			for i,mon in enumerate(mons):
+				species_name = "SPECIES_%s" % mon["species"].upper()
+				if not species_name in defined_species:
+					print("\nerror: species {0} not defined ({1})".format(\
+						species_name,trainer))
+					exit(0)
+				mon["species"] = species_name
+				mons[i] = mon
+				
+			if mon_items and mon_moves:
+				category = "ItemCustomMoves"
+			elif mon_items:
+				category = "ItemDefaultMoves"
+			elif mon_moves:
+				category = "NoItemCustomMoves"
+			else:
+				category = "NoItemDefaultMoves"
+			
+			trainer_data[trainer] = {"pokemon":mons, "category":category}
+
+		mons = []
+		trainer = row[0]
+	elif row[0] != "" and row[0] != "END":
+			if row[0] == "species":
+				columns = row
+			else:
+				mons.append({z[0]:z[1] for z in zip(columns,row)})
+				party = 1
+	# empty rows
+	else:
+		pass
+
+print("done")
+
+# read trainer party file content
+trainer_party_file_lines = []
+trainer_party_file = "{0}/src/data/trainer_parties.h".format(pokeemerald_dir)
+with open(trainer_party_file, "r") as f:
+	for line in f:
+		trainer_party_file_lines.append(line)
+
+# modify trainer and trainer party file lines
+for trainer in trainer_data:
+	trainer_lines = []
+	
+	trainer_lines.append("static const struct TrainerMon{0} {1}[] = {{\n".format(\
+		trainer_data[trainer]["category"],defined_trainers[trainer]))
+
+	trainer_mons = trainer_data[trainer]["pokemon"]
+	for i,mon in enumerate(trainer_mons):
+		trainer_lines.append("    {\n")
+		trainer_lines.append("    .iv = {0},\n".format(mon["iv"]))
+		trainer_lines.append("    .lvl = {0},\n".format(mon["lvl"]))
+		if trainer_data[trainer]["category"] in ["ItemCustomMoves","ItemDefaultMoves"]:
+			trainer_lines.append("    .heldItem = {0},\n".format(mon["heldItem"]))
+		if trainer_data[trainer]["category"] in ["NoItemCustomMoves","ItemCustomMoves"]:
+			trainer_lines.append("    .moves = {%s},\n" % \
+				", ".join(mon["moves"]))
+		trainer_lines.append("    .species = {0},\n".format(mon["species"]))
+		last_line = "    }"
+		if i != len(trainer_mons)-1:
+			last_line += ","
+		last_line += "\n"
+		trainer_lines.append(last_line)
+		
+	trainer_lines.append("};\n")
+	
+	first_index = ""
+	for i,line in enumerate(trainer_party_file_lines):
+		if defined_trainers[trainer] in line:
+			first_index = i
+		elif first_index != "" and line.startswith("static const struct Trainer"):
+			last_index = i-1
+			break
+			
+	trainer_party_file_lines[first_index:last_index] = trainer_lines
+
+	trainer_on = 0
+	for i,line in enumerate(trainer_file_lines):
+		if "[TRAINER_" in line:
+			if trainer in line:
+				trainer_on = 1
+			else:
+				trainer_on = 0
+		if trainer_on:
+			if ".partyFlags" in line:
+				line = "        .partyFlags = "
+				if trainer_data[trainer]["category"] == "ItemCustomMoves":
+					line += "F_TRAINER_PARTY_HELD_ITEM | F_TRAINER_PARTY_CUSTOM_MOVESET"
+				elif trainer_data[trainer]["category"] == "NoItemCustomMoves":
+					line += "F_TRAINER_PARTY_CUSTOM_MOVESET"
+				elif trainer_data[trainer]["category"] == "ItemDefaultMoves":
+					line += "F_TRAINER_PARTY_HELD_ITEM"
+				else:
+					line += "0"
+				line += ",\n"
+				trainer_file_lines[i] = line
+				
+			if ".party = {" in line:
+				line = "        .party = {{.{0} = {1}}},\n".format(\
+					trainer_data[trainer]["category"],defined_trainers[trainer])
+				trainer_file_lines[i] = line
+	
+# write to trainer party file
+with open(trainer_party_file, "w") as f:
+	for line in trainer_party_file_lines:
+		f.write(line)
+
+# write to trainer file
+with open(trainer_file, "w") as f:
+	for line in trainer_file_lines:
+		f.write(line)
+		
 ########## maps
+
+print("\nupdating maps")
 
 os_cmd = "update_maps.py --mode insert"
 
 os.system(os_cmd)
+		
+print("done")		
 		
 ########## finish
 
--- update_maps.py
+++ update_maps.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import os,shutil,argparse
+import os,shutil,argparse,time
 from pathlib import Path
 from config import pokeemerald_dir,vanilla_dir
 
@@ -24,17 +24,23 @@ def mod_to_backup(mod_path):
 
 file_meta = {}
 
+print(map_dir)
+
 for folder in [map_dir,layout_dir]:
 	for dir, subdirs, files in os.walk(folder):
 			for fname in files:
 				
 				relative_path = "{0}\{1}".format(dir,fname).replace(pokeemerald_dir,"")
-				file_meta[relative_path] = {}
 				
 				mod_path = "{0}{1}".format(pokeemerald_dir,relative_path)
 				vanilla_path = "{0}{1}".format(vanilla_dir,relative_path)
 				backup_path = "{0}{1}".format(backup_dir,relative_path)
 				
+				# bugfix?
+				if not os.path.isfile(vanilla_path):
+					continue
+				
+				file_meta[relative_path] = {}
 				file_meta[relative_path]["mod"] = {}
 				file_meta[relative_path]["mod"]["size"] = Path(mod_path).stat().st_size
 				file_meta[relative_path]["mod"]["path"] = mod_path
@@ -60,8 +66,8 @@ if args["mode"] == "insert":
 	print("\ncopy files for porymap")
 
 	for file in file_meta:
+	
 		if file_meta[file]["backup"]["path"] != "":
-			print(file)
 			shutil.copyfile(file_meta[file]["backup"]["path"],\
 				file_meta[file]["mod"]["path"])
 				
@@ -74,12 +80,15 @@ if args["mode"] == "backup":
 	print("\nback up files")
 	
 	for file in file_meta:
+	
 		if (file_meta[file]["mod"]["size"] != file_meta[file]["vanilla"]["size"]) \
 			or (file_meta[file]["mod"]["mod_time"] != file_meta[file]["vanilla"]["mod_time"]):
-			print(file)
+			
 			if file_meta[file]["backup"]["path"] == "":
 				file_meta[file]["backup"]["path"] = mod_to_backup(file_meta[file]["mod"]["path"])
 			backup_path = file_meta[file]["backup"]["path"]
 			os.makedirs(os.path.dirname(backup_path), exist_ok=True)
 			shutil.copy(file_meta[file]["mod"]["path"],backup_path)
 	print("done")
+	
+time.sleep(5)
