4aad26720b92a9796c29cf1d460425773470ab48
root <root@DESKTOP-2V9HLOS.localdomain>
2020-06-11 19:09:11 +0300

    change config paths to be relative, add porymap

--- config.py
+++ config.py
@@ -1,5 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+#!-*- encoding: utf-8 -*-
 
-base_dir = "C:\\Users\Miro\Documents\Pokeemerald\pokeemerald_june20"
-pokeemerald_dir = "{0}\pokeemerald_mod".format(base_dir)
-vanilla_dir = "{0}\pokeemerald".format(base_dir)
+import os
+from pathlib import Path
+
+#base_dir = "C:\\Users\Miro\Documents\Pokeemerald\pokeemerald_june20"
+base_dir = str(Path(__file__).parent.absolute()).rsplit("/",1)[0]
+pokeemerald_dir = "{0}/pokeemerald_mod".format(base_dir)
+vanilla_dir = "{0}/pokeemerald".format(base_dir)
+
+for dir in [pokeemerald_dir,vanilla_dir]:
+	if not os.path.isdir(dir):
+		print("\nerror: did not find directory: %s" % dir)
+		exit(0)
+
