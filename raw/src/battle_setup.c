e3af33f3fcd0865b2dd12d09d2300db21631cf71
Bidoofremix <52993397+Bidoofremix@users.noreply.github.com>
2020-07-16 17:03:10 +0300

    Mewtwo trainer class transition

---
+++ raw/src/battle_setup.c
@@ -0,0 +1,9 @@
+< //
+    if (gTrainers[gTrainerBattleOpponent_A].trainerClass == TRAINER_CLASS_CHAMPION)
+        return B_TRANSITION_CHAMPION;
+    
+	if (gTrainers[gTrainerBattleOpponent_A].trainerClass == TRAINER_CLASS_MEWTWO)
+        return B_TRANSITION_BLACKHOLE1;
+
+    if (gTrainers[gTrainerBattleOpponent_A].trainerClass == TRAINER_CLASS_TEAM_MAGMA
+// >
