467c92cc99b19575bbf2b2a827f3654e8881ad4c
Bidoofremix <52993397+Bidoofremix@users.noreply.github.com>
2020-07-20 10:53:44 +0300

    fix typo

--- new_species.py
+++ new_species.py
@@ -1202,7 +1202,7 @@ direct_sound_file = normalize_path("{0}/sound/direct_sound_data.inc".format(raw_
 with open(direct_sound_file, "w") as f:
 	f.write("< //\n")
 	f.write("Cry_Lycanroc_Dusk::\n")
-	f.write('\t.incbin "sound/direct_sound_samples/cry_not_'lycanroc_dusk.bin"\n')
+	f.write('\t.incbin "sound/direct_sound_samples/cry_not_lycanroc_dusk.bin"\n')
 	for mon in new_mon_national_order:
 		f.write("\t.align 2\n")
 		f.write("Cry_{0}::\n".format(mon.capitalize()))
