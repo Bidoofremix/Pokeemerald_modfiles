fd54efdc8f7ba5c9d9a32c57ecfedafa612c72b5
Bidoofremix <52993397+Bidoofremix@users.noreply.github.com>
2020-07-30 11:20:55 +0300

    altering cave encounters and animation particles back to vanilla

---
+++ raw/include/battle_config.h
@@ -0,0 +1,22 @@
+< //
+#define B_POWDER_GRASS              GEN_6 // In Gen6+, Grass type Pokémon are immune to powder and spore moves.
+
+// Animation Settings
+#define NEW_SWORD_PARTICLE              FALSE    // update swords dance particle
+#define NEW_LEECH_SEED_PARTICLE         FALSE    //update leech seed's animation particle
+#define NEW_HORN_ATTACK_PARTICLE        FALSE    //update horn attack's horn
+#define NEW_LEAF_PARTICLE               FALSE    // update leaf particle
+#define NEW_EMBER_PARTICLES             FALSE    //updates ember fire particle
+#define NEW_MEAN_LOOK_PARTICLE          FALSE    //update mean look eye
+#define NEW_TEETH_PARTICLE              FALSE    //update bite/crunch teeth particle
+#define NEW_HANDS_FEET_PARTICLE         FALSE    //update chop/kick/punch particles
+#define NEW_SPIKES_PARTICLE             FALSE    //update spikes particle
+#define NEW_FLY_BUBBLE_PARTICLE         FALSE    //update fly 'bubble' particle
+#define NEW_CURSE_NAIL_PARTICLE         FALSE    //updates curse nail
+#define NEW_BATON_PASS_BALL_PARTICLE    FALSE    //update baton pass pokeball sprite
+#define NEW_MORNING_SUN_STAR_PARTICLE   FALSE    //updates morning sun star particles
+#define NEW_IMPACT_PALETTE              FALSE    //updates the basic 'hit' particle
+#define NEW_SURF_PARTICLE_PALETTE       FALSE    //updates the surf wave palette
+
+#endif // GUARD_CONSTANTS_BATTLE_CONFIG_H
+// >
