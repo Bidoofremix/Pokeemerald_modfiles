254355f71699ed318b67525ccd714ac25490f99e
Bidoofremix <Bidoofremix@gmail.com>
2020-08-04 21:21:50 +0300

    fix shiny odd snippet breaking things

--- raw/include/constants/pokemon.h
+++ raw/include/constants/pokemon.h
@@ -1,8 +1,9 @@
 < //
-#define NUM_BATTLE_STATS NUM_STATS + 2 // includes Accuracy and Evasion
+#define SPLIT_STATUS    0x2
 
 // Shiny odds
 #define SHINY_ODDS 512 // Actual probability is SHINY_ODDS/65536
+#define SHINY_CHARM_REROLLS 3 // Amount of re-rolls if has Shiny Charm.
 
 // Flags for Get(Box)MonData / Set(Box)MonData
 #define MON_DATA_PERSONALITY        0
