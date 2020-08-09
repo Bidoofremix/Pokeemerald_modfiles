4bbaf060e6c8d5cf43df92d701357f481bfc8c77
Bidoofremix <Bidoofremix@gmail.com>
2020-08-09 19:17:50 +0300

    try to figure out where the missing definition for silent forest map scripts goes

--- raw/data/event_scripts.s
+++ raw/data/event_scripts.s
@@ -2,6 +2,7 @@
 	.include "data/maps/Route124_DivingTreasureHuntersHouse/scripts.inc"
 
 	.include "data/maps/VerdanturfTown_BreedersClub/scripts.inc"
+	.include "data/maps/SilentForest/scripts.inc"
 
 	.include "data/scripts/std_msgbox.inc"
 // >
