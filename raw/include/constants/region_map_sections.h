844d19cbea85ac53e5566f5413027cef560327cb
Bidoofremix <Bidoofremix@gmail.com>
2020-08-09 17:28:26 +0300

    add silent forest map base

---
+++ raw/include/constants/region_map_sections.h
@@ -0,0 +1,7 @@
+< //
+#define MAPSEC_TRAINER_HILL                 0xD4
+#define MAPSEC_SILENT_FOREST                0xD5
+#define MAPSEC_NONE                         0xD6
+
+#define METLOC_SPECIAL_EGG                  0xFD
+// >
---
+++ raw/src/data/region_map/region_map_entries.h
@@ -0,0 +1,12 @@
+< //
+static const u8 sMapName_TrainerHill[] = _("TRAINER HILL");
+static const u8 sMapName_SilentForest[] = _("SILENT FOREST");
+
+const struct RegionMapLocation gRegionMapEntries[] = {
+// >
+
+< //
+    [MAPSEC_NAVEL_ROCK]                = { 0,  0, 1, 1, sMapName_NavelRock},
+    [MAPSEC_SILENT_FOREST]             = { 4, 12, 1, 1, sMapName_SilentForest},
+    [MAPSEC_TRAINER_HILL]              = { 8,  4, 1, 1, sMapName_TrainerHill}
+// >
