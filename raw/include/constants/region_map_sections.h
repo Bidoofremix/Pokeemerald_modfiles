27627f258f46b51ad2c89f5617bfd9441dc94c0a
Bidoofremix <Bidoofremix@gmail.com>
2020-08-22 08:11:11 +0300

    add new map section for brine beach

--- raw/include/constants/region_map_sections.h
+++ raw/include/constants/region_map_sections.h
@@ -1,7 +1,8 @@
 < //
 #define MAPSEC_TRAINER_HILL                 0xD4
 #define MAPSEC_SILENT_FOREST                0xD5
-#define MAPSEC_NONE                         0xD6
+#define MAPSEC_BRINE_BEACH                  0xD6
+#define MAPSEC_NONE                         0xD7
 
 #define METLOC_SPECIAL_EGG                  0xFD
 // >
--- raw/src/data/region_map/region_map_entries.h
+++ raw/src/data/region_map/region_map_entries.h
@@ -1,6 +1,7 @@
 < //
 static const u8 sMapName_TrainerHill[] = _("TRAINER HILL");
 static const u8 sMapName_SilentForest[] = _("SILENT FOREST");
+static const u8 sMapName_BrineBeach[] = _("BRINE BEACH");
 
 const struct RegionMapLocation gRegionMapEntries[] = {
 // >
@@ -8,5 +9,6 @@ const struct RegionMapLocation gRegionMapEntries[] = {
 < //
     [MAPSEC_NAVEL_ROCK]                = { 0,  0, 1, 1, sMapName_NavelRock},
     [MAPSEC_SILENT_FOREST]             = { 4, 12, 1, 1, sMapName_SilentForest},
+    [MAPSEC_BRINE_BEACH]             = { 4, 12, 1, 1, sMapName_BrineBeach},
     [MAPSEC_TRAINER_HILL]              = { 8,  4, 1, 1, sMapName_TrainerHill}
 // >
