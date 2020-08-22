74395f9644de46b08ac2985ce62ba3b43fe8a557
Bidoofremix <Bidoofremix@gmail.com>
2020-08-22 07:53:46 +0300

    try to add brine beach again

--- raw/data/event_scripts.s
+++ raw/data/event_scripts.s
@@ -5,6 +5,7 @@
 	.include "data/maps/SilentForest/scripts.inc"
 	.include "data/maps/RustboroCity_University_GreatHall/scripts.inc"
 	.include "data/maps/RustboroCity_University_PostDocRoom/scripts.inc"
+	.include "data/maps/BrineBeach/scripts.inc"
 
 	.include "data/scripts/std_msgbox.inc"
 // >
