8d83cfab9de5d6538b9d876e44c70ab31e15e325
Bidoofremix <Bidoofremix@gmail.com>
2020-08-22 08:18:39 +0300

    try to fix map name popups

--- raw/src/data/region_map/region_map_entries.h
+++ raw/src/data/region_map/region_map_entries.h
@@ -9,6 +9,6 @@ const struct RegionMapLocation gRegionMapEntries[] = {
 < //
     [MAPSEC_NAVEL_ROCK]                = { 0,  0, 1, 1, sMapName_NavelRock},
     [MAPSEC_SILENT_FOREST]             = { 4, 12, 1, 1, sMapName_SilentForest},
-    [MAPSEC_BRINE_BEACH]             = { 4, 12, 1, 1, sMapName_BrineBeach},
+    [MAPSEC_BRINE_BEACH]               = { 4, 12, 1, 1, sMapName_BrineBeach},
     [MAPSEC_TRAINER_HILL]              = { 8,  4, 1, 1, sMapName_TrainerHill}
 // >
