5377814b1d626e4a13c1602eb2e48ff1fe7791b6
Bidoofremix <Bidoofremix@gmail.com>
2020-08-23 19:01:21 +0300

    surfers hut scripts and texts

--- raw/include/constants/flags.h
+++ raw/include/constants/flags.h
@@ -60,7 +60,7 @@
 #define FLAG_HIDE_GYARADOS                     0x2F // Unused Flag
 #define FLAG_HIDE_BRINE_BEACH_TEAM_AQUA        0x30 // Unused Flag
 #define FLAG_INTRODUCED_ABILITY_CHANGER        0x31 // Unused Flag
-#define FLAG_UNUSED_0x054                    0x32  // Unused Flag
+#define FLAG_INTRODUCED_SURFER_TUTOR           0x32  // Unused Flag
 #define FLAG_UNUSED_0x055                    0x33  // Unused Flag
 #define FLAG_UNUSED_0x068                    0x34  // Unused Flag
 #define FLAG_UNUSED_RS_LEGENDARY_BATTLE_DONE 0x35 // Unused Flag. Used in R/S to indicate whether player defeated or caught Groudon/Kyogre in Cave of Origin.
