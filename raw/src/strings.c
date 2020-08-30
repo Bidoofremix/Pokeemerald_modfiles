61907d648f02c79f30d7d34ad94524b5313a0dc2
Bidoofremix <Bidoofremix@gmail.com>
2020-08-30 14:19:03 +0300

    adjust stuff in rustboro

--- raw/src/strings.c
+++ raw/src/strings.c
@@ -7,6 +7,6 @@ const u8 gText_CantBuyKeyItem[] = _("{STR_VAR_2}? Oh, no.\nI can't buy that.{PAU
 
 < //
 const u8 gText_PeculiarEggTrade[] = _("A peculiar POKéMON EGG\nobtained in a trade.");
-const u8 gText_EggFromBreedersClub[] = _("A POKéMON EGG obtained\nat BREEDER's CLUB.");
+const u8 gText_EggFromBreedersClub[] = _("A POKéMON EGG obtained\nat the BREEDER's CLUB.");
 const u8 gText_EggFromHotSprings[] = _("A POKéMON EGG obtained\nat the hot springs.");
 // >
