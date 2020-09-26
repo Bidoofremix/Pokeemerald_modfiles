3065bd13f72155af3f421ec06bde73ca39348956
Bidoofremix <Bidoofremix@gmail.com>
2020-09-26 21:45:26 +0300

    more game corner stuff

---
+++ raw/include/constants/script_menu.h
@@ -0,0 +1,8 @@
+< //
+#define MULTI_TAG_MATCH_TYPE               113
+#define MULTI_GAME_CORNER_STONES_1         114
+#define MULTI_GAME_CORNER_STONES_2         115
+
+// Lilycove SS Tidal Multichoice Selections
+#define SSTIDAL_SELECTION_SLATEPORT        0
+// >
--- raw/src/data/script_menu.h
+++ raw/src/data/script_menu.h
@@ -1,18 +1,41 @@
 < //
 static const struct MenuAction MultichoiceList_GameCornerTMs[] =
 {
+    {gText_TM32AndPrice},
+    {gText_TM29AndPrice},
+    {gText_TM35AndPrice},
+    {gText_TM24AndPrice},
+    {gText_TM13AndPrice},
+    {gText_Exit},
+};
+
+static const struct MenuAction MultichoiceList_GameCornerStones1[] =
+{
     {gText_FireStoneAndPrice},
     {gText_WaterStoneAndPrice},
     {gText_ThunderStoneAndPrice},
     {gText_LeafStoneAndPrice},
-    {gText_MoonStoneAndPrice},
-    {gText_SunStoneAndPrice},
-    {gText_ShinyStoneAndPrice},
-    {gText_DuskStoneAndPrice},
-    {gText_DawnStoneAndPrice},
     {gText_IceStoneAndPrice},
     {gText_Exit},
 };
 
+static const struct MenuAction MultichoiceList_GameCornerStones2[] =
+{
+    {gText_SunStoneAndPrice},
+    {gText_MoonStoneAndPrice},
+    {gText_ShinyStoneAndPrice},
+    {gText_DuskStoneAndPrice},
+    {gText_DawnStoneAndPrice},
+    {gText_Exit},
+};
+
 static const struct MenuAction MultichoiceList_GameCornerCoins[] =
+// >
+
+
+< //
+    [MULTI_GAME_CORNER_TMS]            = MULTICHOICE(MultichoiceList_GameCornerTMs),
+    [MULTI_GAME_CORNER_STONES_1]       = MULTICHOICE(MultichoiceList_GameCornerStones1),
+    [MULTI_GAME_CORNER_STONES_2]       = MULTICHOICE(MultichoiceList_GameCornerStones2),
+    [MULTI_SSTIDAL_SLATEPORT_NO_BF]    = MULTICHOICE(MultichoiceList_SSTidalSlateportNoBF),
 // >
