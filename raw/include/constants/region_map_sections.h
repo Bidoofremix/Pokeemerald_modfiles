bfefe01908627657d12e047ec483c6d9c1efe98a
Bidoofremix <Bidoofremix@gmail.com>
2020-09-29 19:36:40 +0300

    new map sections

--- raw/include/constants/region_map_sections.h
+++ raw/include/constants/region_map_sections.h
@@ -2,6 +2,9 @@
 #define MAPSEC_TRAINER_HILL                 0xD4
 #define MAPSEC_SILENT_FOREST                0xD5
 #define MAPSEC_BRINE_BEACH                  0xD6
+#define MAPSEC_MYSTERY_SPRINGS              0xD8
+#define MAPSEC_NEW_ISLAND                   0xD9
+#define MAPSEC_NEW_ISLAND2                  0xDA
 #define MAPSEC_NONE                         0xD7
 
 #define METLOC_BREEDERSCLUB_EGG             0xFC
--- raw/src/data/region_map/region_map_entries.h
+++ raw/src/data/region_map/region_map_entries.h
@@ -2,6 +2,9 @@
 static const u8 sMapName_TrainerHill[] = _("TRAINER HILL");
 static const u8 sMapName_SilentForest[] = _("SILENT FOREST");
 static const u8 sMapName_BrineBeach[] = _("BRINE BEACH");
+static const u8 sMapName_MysterySprings[] = _("MYSTERY SPRINGS");
+static const u8 sMapName_NewIsland[] = _("NEW ISLAND");
+static const u8 sMapName_NewIsland2[] = _("NEW ISLAND");
 
 const struct RegionMapLocation gRegionMapEntries[] = {
 // >
@@ -10,5 +13,8 @@ const struct RegionMapLocation gRegionMapEntries[] = {
     [MAPSEC_NAVEL_ROCK]                = { 0,  0, 1, 1, sMapName_NavelRock},
     [MAPSEC_SILENT_FOREST]             = { 4, 12, 1, 1, sMapName_SilentForest},
     [MAPSEC_BRINE_BEACH]               = { 4, 12, 1, 1, sMapName_BrineBeach},
+    [MAPSEC_MYSTERY_SPRINGS]           = { 4, 12, 1, 1, sMapName_MysterySprings},
+    [MAPSEC_NEW_ISLAND]                = { 4, 12, 1, 1, sMapName_NewIsland},
+    [MAPSEC_NEW_ISLAND2]               = { 4, 12, 1, 1, sMapName_NewIsland2},
     [MAPSEC_TRAINER_HILL]              = { 8,  4, 1, 1, sMapName_TrainerHill}
 // >
