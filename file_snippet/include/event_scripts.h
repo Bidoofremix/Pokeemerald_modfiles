81baab0c64bbe508cc9810cea40f6082952bc46f
Bidoofremix <Bidoofremix@gmail.com>
2020-10-17 13:31:59 +0300

    prof stone match call

---
+++ file_snippet/include/event_scripts.h
@@ -0,0 +1,5 @@
+< //
+extern const u8 MauvilleCity_EventScript_RegisterWallyCall[];
+extern const u8 MauvilleCity_EventScript_ProfStoneCall[];
+extern const u8 Route119_EventScript_ScottWonAtFortreeGymCall[];
+// >
