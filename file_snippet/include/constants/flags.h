98efbd7c22d65266ce60577dd86bba6e4e615597
Bidoofremix <Bidoofremix@gmail.com>
2020-11-15 11:32:40 +0200

    work on skitty quest

--- file_snippet/include/constants/flags.h
+++ file_snippet/include/constants/flags.h
@@ -78,7 +78,7 @@
 #define FLAG_HIDE_FALLARBOR_TOWN_DRYSTAN       0x41 // Unused Flag
 #define FLAG_REVEIVED_FOOTPRINTS_MAN_REVIVE    0x42 // Unused Flag
 #define FLAG_HIDE_ROUTE115_SKITTY              0x43 // Unused Flag
-#define FLAG_UNUSED_0x268  0x44 // Unused Flag
+#define FLAG_HIDE_ROUTE115_OLDLADY             0x44 // Unused Flag
 #define FLAG_UNUSED_0x269  0x45 // Unused Flag
 #define FLAG_UNUSED_0x26A  0x46 // Unused Flag
 #define FLAG_UNUSED_0x26B  0x47 // Unused Flag
