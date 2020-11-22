75b75aaef3b41c01e0e1dfb9706bac63d8eadff4
Bidoofremix <Bidoofremix@gmail.com>
2020-11-22 09:42:19 +0200

    plural giveitem

--- file_snippet/src/item.c
+++ file_snippet/src/item.c
@@ -13,3 +13,21 @@ void ItemId_GetHoldEffectParam_Script()
 
 static void SetBagItemQuantity(u16 *quantity, u16 newValue)
 // >
+
+< //
+void CopyItemNameHandlePlural(u16 itemId, u8 *dst, u32 quantity)
+{
+    StringCopy(dst, ItemId_GetName(itemId));
+    if (quantity > 1)
+    {
+        if (ItemId_GetPocket(itemId) == POCKET_BERRIES)
+            GetBerryCountString(dst, gBerries[itemId - ITEM_CHERI_BERRY].name, quantity);
+        else
+            StringAppend(dst, sText_s);
+    }
+}
+
+static const u8 sText_s[] = _("s");
+
+void GetBerryCountString(u8 *dst, const u8 *berryName, u32 quantity)
+// >
