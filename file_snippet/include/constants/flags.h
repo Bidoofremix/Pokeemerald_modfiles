e930c36af56d62e70f09cc27e01fe813d372d32d
Bidoofremix <Bidoofremix@gmail.com>
2020-11-22 09:34:03 +0200

    finalize skitty quest

--- file_snippet/include/constants/flags.h
+++ file_snippet/include/constants/flags.h
@@ -79,7 +79,7 @@
 #define FLAG_REVEIVED_FOOTPRINTS_MAN_REVIVE    0x42 // Unused Flag
 #define FLAG_HIDE_ROUTE115_SKITTY              0x43 // Unused Flag
 #define FLAG_HIDE_ROUTE115_OLDLADY             0x44 // Unused Flag
-#define FLAG_UNUSED_0x269  0x45 // Unused Flag
+#define FLAG_RECEIVED_OLD_LADY_MOON_BALLS      0x45 // Unused Flag
 #define FLAG_UNUSED_0x26A  0x46 // Unused Flag
 #define FLAG_UNUSED_0x26B  0x47 // Unused Flag
 #define FLAG_UNUSED_0x26C  0x48 // Unused Flag
